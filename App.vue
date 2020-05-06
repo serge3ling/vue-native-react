c80d082e1a72a1257b5150eb758ee4d2c0ffc9af
Sergio Trett <serge3ling@gmail.com>
2020-05-06 17:23:53 +0300

    .gitignore for Gradle.

--- App.vue
+++ App.vue
@@ -1,8 +1,41 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
+    <status-bar background-color="grey" bar-style="light-content" />
+    <text class="text-color-primary">{{ message }}</text>
+    <button v-bind:title="message" v-on:press="handleBtnPress"/>
+    <switch v-model="switchVal" />
+    <todo-item class="text-container" v-for="todo in todos"
+      v-bind:key="todo.id"
+      v-bind:item="todo"
+    />
   </view>
 </template>
+
+<script type="text/javascript">
+import TodoItem from './components/todoItem.vue';
+
+export default {
+  components: { TodoItem },
+
+  data() {
+    return {
+      message: 'Ahoj svet',
+      switchVal: false,
+      todos: [
+        { id: 1, text: 'Learn JavaScript' },
+        { id: 2, text: 'Learn Vue' },
+        { id: 3, text: 'Build something awesome' },
+      ],
+    };
+  },
+
+  methods: {
+    handleBtnPress() {
+      alert('Btn Press.');
+    },
+  },
+}
+</script>
 
 <style>
 .container {
@@ -11,7 +44,19 @@
   justify-content: center;
   flex: 1;
 }
+.text-container {
+  color: blue;
+  padding: 2;
+  font-size: 22;
+}
+.text-input-container {
+  width: 300;
+  height: 40;
+  font-size: 22;
+  border-color: gray;
+}
 .text-color-primary {
   color: blue;
+  font-size: 30;
 }
 </style>
---
+++ components/todoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <text>
+    {{ item.id }}. {{ item.text }}
+  </text>
+</template>
+
+<script type="text/javascript">
+export default {
+  props: {
+    item: {
+      Type: Object,
+    }
+  }
+};
+</script>
